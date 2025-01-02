476cc8ccde925e387cc66e9bde399b52849944ec
viniciussoftsy <168453902+viniciussoftsy@users.noreply.github.com>
2025-01-02 12:56:34 -0300

    Mudanças para subir o pct

--- WebContent/resources/assets/js/matrizCurricular/alunos.js
+++ WebContent/resources/assets/js/matrizCurricular/alunos.js
@@ -164,12 +164,13 @@ function listarDados(dados) {
 			'<span style="width:50%; margin-right: 5px; height: 31px; padding: 8px; display: flex; align-items: center; justify-content: center;" class="btn btn-primary btn-sm" ' +
 			'data-id=' + item.idAluno +
 			' onclick="showModal(this)"><i class="fa-solid fa-file-lines "></i></span>' +
+			/*  BOTÃO AVISOWS DO ALUNO
+			
 			'<span style="width:50%; margin-right: 5px; height: 31px; padding: 8px; display: flex; align-items: center; justify-content: center;" class="btn btn-primary btn-sm" ' +
 			'data-id=' + item.idAluno +
 			' onclick="verAvisosAluno(this)"><i class="fa-solid fa-bell"></i></span>' +
-			/*			'<span style="width: 63px; margin-right: 5px; height: 31px; padding: 8px; display: flex; align-items: center; justify-content: center;" class="btn btn-warning btn-sm" data-id="' +
-						item.idAluno +
-						'" onclick="showModal(this)"><i class="fa-solid fa-pen fa-lg"></i></span>' +*/
+			*/
+			
 			'</td>' +
 			"</tr>"
 		);
--- WebContent/resources/assets/js/matrizCurricular/dadosAluno.js
+++ WebContent/resources/assets/js/matrizCurricular/dadosAluno.js
@@ -95,7 +95,7 @@ const getDadosAcademicos = () => {
 };
 
 const getDadosAluno = () => {
-	$.ajax({
+	/*$.ajax({
 		url: url_base + "/nota/aluno/" + idAluno,
 		type: "GET",
 		async: false,
@@ -110,7 +110,7 @@ const getDadosAluno = () => {
 	}).done(function(res) {
 		notas = res;
 		listarNotas(res);
-	});
+	});*/
 
 	$.ajax({
 		url: url_base + "/alunos/" + idAluno,
@@ -365,7 +365,7 @@ const getDadosAluno = () => {
 	$("isEnderecoAluno").hide();
 };
 
-const listarNotas = (dados) => {
+/*const listarNotas = (dados) => {
 	dados.sort((a, b) => a.ordem - b.ordem);
 
 	const formatarData = (data) => {
@@ -436,7 +436,7 @@ const listarNotas = (dados) => {
 		.join("");
 
 	$("#cola-tabela-notas").html(html);
-};
+};*/
 var idNota = "";
 var notaSelecionada = [];
 
